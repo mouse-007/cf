403b1d064978b2f891f7eb0401b516ba0774c302
何伟 <1096207423@qq.com>
2020-12-11 18:33:56 +0800

    去掉fs

--- extension.js
+++ extension.js
@@ -1,7 +1,6 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 const vscode = require('vscode');
-const fs = require("fs")
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
@@ -18,21 +17,18 @@ function activate(context) {
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with  registerCommand
 	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('helloworld.helloWorld', function (e) {
+	let disposable = vscode.commands.registerCommand('helloworld.helloWorld', async function (e) {
 		// The code you place here will be executed every time your command is executed
 
 		// Display a message box to the user
 		// vscode.window.showErrorMessage('皇帝哈德哈韩的');
 		// 创建文件
-		vscode.window.showInputBox({
+		const name = await vscode.window.showInputBox({
 			placeHolder: '请输入文件名字',
 			prompt: "请输入要创建的文件的名字",
-		}).then(name => {
-			fs.mkdirSync(e.fsPath + `\\${name}`);
-			fs.writeFileSync(e.fsPath + `\\${name}\\${name}.js`, "hello world", { flag: 'w+' });
-		});
-
-
+		})
+		vscode.workspace.fs.createDirectory(e.fsPath + `\\${name}`);
+		vscode.workspace.fs.writeFile(e.fsPath + `\\${name}\\${name}.js`, "hello world");
 	});
 
 	context.subscriptions.push(disposable);
